f005547a259b31f02e441a8eaf8d71042f3ecb90
Alejandro.Marin <4asjmamt@gmail.com>
2026-01-16 15:46:06 +0100

    add controller and route to get userbyID

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -30,7 +30,20 @@ const editUserById = async (req, res) => {
   }
 };
 
+const getUserById = async (req, res) => {
+  try {
+    const { idUser } = req.params;
+    console.log(idUser);
+    const user = await userModel.findById(idUser);
+    if (!user) return res.status(200).send("No existe usuario con ese id");
+    res.status(200).send({ status: "Success", data: user });
+  } catch (error) {
+    res.status(500).send({ status: "Failed", error: error.message });
+  }
+};
+
 module.exports = {
   deleteUserById,
   editUserById,
+  getUserById
 };
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { deleteUserById, editUserById} = require('../controllers/userController');
+const { deleteUserById, editUserById, getUserById} = require('../controllers/userController');
 const verifyToken = require('../middlewares/verifyToken');
 
 // Ruta para eliminar un usuario por su ID
@@ -9,5 +9,8 @@ router.delete('/user/:idUser', verifyToken, deleteUserById);
 
 // Ruta para editar un usuario por su ID
 router.put('/user/:idUser', verifyToken, editUserById);
+
+// Ruta para obtener un usuario por su ID
+router.get('/user/:idUser', verifyToken, getUserById);
 
 module.exports = router;
